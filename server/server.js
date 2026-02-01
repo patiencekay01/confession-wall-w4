2f6ddfacc1db744018e9412b44f9622ea7da762c
PATIENCE KANDARE <PATIENCE.KANDARE@HOTMAIL.COM>
2026-02-01 20:28:54 +0000

    add server side

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import express, { response } from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import pg from "pg"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+dotenv.config()
+
+const db = new pg.Pool({
+    connectionString: process.env.DB_CONN
+})
+
+app.get('/', (request, response) => {
+    response.json('Connected')
+})
+
+app.get('/confessions', async (req, res) => {
+    const data = await db.query(`SELECT * FROM confessions`)
+    const confessions = data.rows
+    response.status(200).json(confessions)
+})
+
+app.post('/confessions', async (request, response) => {
+    const input = request.body
+
+const insertMessages = await db.query(`INSERT INTO confessions (display_name, messages) VALUES ($1, $2)`, [input.displayName, input.messages])
+
+response.status(200).json({alert: "confession added!"})
+})
+
+app.listen(3000, () => {
+    console.log(`Serving running on http://localhost:3000`)
+})
